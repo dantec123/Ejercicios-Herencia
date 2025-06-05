9f3daa4bc085c91a4e066da20633bf10290d913e
dantec123 <dante.corzo.081@alu.tecnica29de6.edu.ar>
2025-06-04 22:03:04 -0300

    ejercicio 2.py

--- ejercicio 2.py
+++ ejercicio 2.py
@@ -4,36 +4,28 @@ class Empleado:
         self.salario = salario
         self.cargo = cargo
 
-    def __str__(self):
-        return f"Nombre: {self.nombre}, Salario: {self.salario}, Cargo: {self.cargo}"
-
-    def calcular_aumento(self):
-
-        raise NotImplementedError("Debe implementarse en las subclases.")
+    def mostrar_info(self):
+        print(f"Nombre: {self.nombre}, Cargo: {self.cargo}, Salario: {self.salario}")
 
 
 class Gerente(Empleado):
-    def __init__(self, nombre, salario):
-        super().__init__(nombre, salario, "Gerente")
-
     def calcular_aumento(self):
-   
-        if self.salario < 1000:
-            aumento = self.salario * 0.15
-        else:
-            aumento = self.salario * 0.10
-        return self.salario + aumento
+        return self.salario * 1.20
 
 
 class EmpleadoTemporal(Empleado):
-    def __init__(self, nombre, salario, meses_contratado):
-        super().__init__(nombre, salario, "Empleado Temporal")
-        self.meses_contratado = meses_contratado
-
     def calcular_aumento(self):
-    
-        if self.meses_contratado > 4:
-            aumento = self.salario * 0.06
-        else:
-            aumento = self.salario * 0.03
-        return self.salario + aumento
+        return self.salario * 1.05
+
+
+if __name__ == "__main__":
+    g = Gerente("Pedro", 5698, "Gerente")
+    et = EmpleadoTemporal("Juan", 2500, "Temporal")
+
+    g.mostrar_info()
+    print("Nuevo salario Gerente:", g.calcular_aumento())
+
+
+
+    et.mostrar_info()
+    print("Nuevo salario Empleado Temporal:", et.calcular_aumento())
